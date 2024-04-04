fe2c2ef8bf372833a4d82350f928f719b4cd0518
Dayana Molina <Molinadayana.ed@gmail.com>
2024-04-03 23:02:22 -0300

    se sube PreEntrega2

--- js/main.js
+++ js/main.js
@@ -1,112 +1,156 @@
-// function aplicarDcto() {
-
-//     let nombre = prompt("Ingrese su nombre");
-//     let precio;
-//     let cursoLenceria = 1
-//     let cursoBikinis = 2
-//     let efectivo = 1
-//     let tarjeta = 2
-
-//     if (nombre) {
-//         alert("Bienvenido " + nombre + "!");
-
-//         while (true) {
-//             let productos = prompt("Ingrese el número del curso: 1)Lencería, 2)Bikinis")
-
-//             if (parseInt(productos) === cursoLenceria) {
-//                 alert("El valor del curso es de $20000");
-//                 precio = 20000;
-//                 break;
-//             }
-//             if (parseInt(productos) === cursoBikinis) {
-//                 alert("El valor del curso es de $25000");
-//                 precio = 25000;
-//                 break
-//             }
-//             else {
-//                 alert("Opcion incorrecta, ingrese el Nº 1 para Lencería o el Nº 2 para Bikinis.") 
-//             }
-
-//         }
-
-//         while (true) {
-//             metodoPago = prompt("Ingrese el número del método de pago: 1)Efectivo, 2)Tarjeta");
-//             if (parseInt(metodoPago) === efectivo || parseInt(metodoPago) === tarjeta) {
-//                 break;
-//             } else {
-//                 alert("Opcion incorrecta, ingrese el Nº 1 para Efectivo o el Nº 2 para Tarjeta.");
-//             }
-//         }
-
-
-//         if (parseInt(metodoPago) === efectivo) {
-//             precio -= (precio * 0.20);
-//             alert("Se aplicó un descuento del 20% por pago en efectivo. El monto a pagar es: $" + precio.toFixed(2));
-//         } else {
-//             precio += (precio * 0.10);
-//             alert("Se incrementó un 10% por pago con tarjeta. El monto a pagar es: $" + precio.toFixed(2));
-//         }
-//     }
-// }
-// aplicarDcto()
-
-
-
-
-
-
-// let nombreCurso = prompt("Ingrese el nombre del producto seleccionado");
-// let precioCurso = prompt("Ingrese el precio del producto seleccionado");
-// let categoriaCurso = prompt("Ingrese la categoria del producto seleccionado");
-
-// let curso = {
-//     nombre: nombreCurso,
-//     precio: precioCurso,
-//     categoria: categoriaCurso,
-// }
-
-// let carritoCursos = [];
-
-// carritoCursos.push(curso);
-
-// console.log(carritoCursos);
-
-
-
-
-
-// se crea variable que indica si se sale del ciclo while o no
-let salir = false
-
-// Se crea el array de cursos 
-let carritoCursos = [];
-
-// Se crea ciclo while con la condicion de que mientras salir sea false el ciclo continue
-while (!salir) {
-
-    //Se obtienen los datos del curso
-    let nombreCurso = prompt("Ingrese el nombre del producto seleccionado");
-    let precioCurso = prompt("Ingrese el precio del producto seleccionado");
-    let categoriaCurso = prompt("Ingrese la categoria del producto seleccionado");
-
-    //Se crea el objeto curso
-    let curso = {
-        nombre: nombreCurso,
-        precio: precioCurso,
-        categoria: categoriaCurso,
+function obtenerNombre() {
+    let nombre = prompt("Ingrese su nombre");
+    //Forma de validar que se ingrese solo letras
+    let soloLetras = /^[A-Za-z]+$/;
+    while (!soloLetras.test(nombre)) {
+        nombre = prompt("El nombre debe contener solo letras. Ingrese su nombre nuevamente:");
     }
+    return nombre;
+}
 
-    //Se añade el objeto curso al array de cursos
-    carritoCursos.push(curso);
+function obtenerCurso() {
+    let curso = prompt("Ingrese el número del curso: \n1) Lencería\n2) Bikinis\n3) Corsetería\n4) Costura Inicial\n5) Moldería Base");
+    //Forma de validar que se ingrese solo numeros
+    let soloNumeros = /^\d+$/;
+    while (!soloNumeros.test(curso)) {
+        curso = prompt("Debe ingresar sólo el Nº del curso.  \n1) Lencería\n2) Bikinis\n3) Corsetería\n4) Costura Inicial\n5) Moldería Base");
+    }
+    return curso;
+}
 
-    //Se pregunta al usuario si quiere salir o continuar cargando cursos
-    let quiereSalir = prompt("Seleccione 1 si quiere salir y seleccione 2 si desea continuar")
+function obtenerMetodoDePago() {
+    let metodoPago = prompt("Ingrese el número del método de pago: 1)Efectivo, 2)Tarjeta");
+    //Forma de validar que se ingrese solo numeros
+    let soloNumeros = /^\d+$/;
+    while (!soloNumeros.test(metodoPago)) {
+        metodoPago = prompt("Debe ingresar sólo el Nº del curso. Ingrese el número del método de pago: 1)Efectivo, 2)Tarjeta");
+    }
+    return metodoPago;
+}
 
-    //Si selecciona 1 se cambia el valor de la variable salir a true para terminar de cargar los cursos y salir del ciclo while
-    if (quiereSalir === "1") {
-        salir = true
+function deseaContinuar() {
+    let deseaElegirOtroCurso = prompt("Desea cargar otro curso? 1) Si, 2) No.")
+    if (deseaElegirOtroCurso === "2") {
+        return false
+    } else {
+        return true
     }
 }
 
-console.log(carritoCursos);
+function seleccionarCursos() {
+    let cursoLenceria = 1
+    let cursoBikinis = 2
+    let cursoCorseteria = 3
+    let cursoCosturaInicial = 4
+    let cursoMolderiaBase = 5
+    let continuar = true
 
+    let cursos = []
+
+    while (continuar) {
+        let curso = obtenerCurso();
+
+        if (parseInt(curso) === cursoMolderiaBase || parseInt(curso) === cursoBikinis || parseInt(curso) === cursoCosturaInicial || parseInt(curso) === cursoCorseteria || parseInt(curso) === cursoLenceria) {
+            if (parseInt(curso) === cursoLenceria) {
+                let cursoElegido = {
+                    nombre: "Curso Lenceria",
+                    precio: 20000
+                }
+                cursos.push(cursoElegido)
+                continuar = deseaContinuar()
+            }
+
+            if (parseInt(curso) === cursoBikinis) {
+                let cursoElegido = {
+                    nombre: "Curso Biknis",
+                    precio: 25000
+                }
+                cursos.push(cursoElegido)
+                continuar = deseaContinuar()
+            }
+
+            if (parseInt(curso) === cursoCorseteria) {
+                let cursoElegido = {
+                    nombre: "Curso Corseteria",
+                    precio: 28000
+                }
+                cursos.push(cursoElegido)
+                continuar = deseaContinuar()
+            }
+
+            if (parseInt(curso) === cursoCosturaInicial) {
+                let cursoElegido = {
+                    nombre: "Curso Costura Inicial",
+                    precio: 17000
+                }
+                cursos.push(cursoElegido)
+                continuar = deseaContinuar()
+            }
+
+            if (parseInt(curso) === cursoMolderiaBase) {
+                let cursoElegido = {
+                    nombre: "Curso Molderia Base",
+                    precio: 19000
+                }
+                cursos.push(cursoElegido)
+                continuar = deseaContinuar()
+            }
+        } else {
+            alert("Opcion incorrecta, Debe ingresar sólo el Nº del curso.  \n1) Lencería\n2) Bikinis\n3) Corsetería\n4) Costura Inicial\n5) Moldería Base.")
+        }
+    }
+    return cursos
+}
+
+function obtenerSubtotal(cursos) {
+    let subtotal = 0
+    cursos.forEach(curso => {
+        subtotal += curso.precio
+    })
+    return subtotal
+}
+
+function comprarCursos() {
+
+    let nombre = obtenerNombre();
+    let precio;
+    let efectivo = 1
+    let tarjeta = 2
+
+    if (nombre) {
+        alert("Bienvenido " + nombre + "!");
+
+        const carritoCursos = seleccionarCursos();
+        console.log(carritoCursos);
+
+        //Mostrar listado de cursos seleccionados
+        let listaCursos = ""
+        carritoCursos.forEach(curso => {
+            listaCursos += "-" + curso.nombre + "\n"
+        })
+        alert("Lista de cursos:\n" + listaCursos);
+
+        //Mostrar el curso mas caro
+        let precios = carritoCursos.map(curso => curso.precio);
+        let precioMaximo = Math.max(...precios);
+        let cursoMasCaro = carritoCursos.find(curso => curso.precio === precioMaximo);
+        alert("El curso más caro es: " + cursoMasCaro.nombre + " con un precio de $" + cursoMasCaro.precio);
+
+        let metodoPago = obtenerMetodoDePago();
+
+        let subtotal = obtenerSubtotal(carritoCursos)
+        let total = 0
+
+        if (parseInt(metodoPago) === efectivo) {
+            total = (subtotal * 0.80);
+            alert("Se aplicó un descuento del 20% por pago en efectivo. El monto a pagar es: $" + total.toFixed(2));
+            alert("Gracias por su compra")
+        } else {
+            total = (subtotal * 0.90);
+            alert("Se incrementó un 10% por pago con tarjeta. El monto a pagar es: $" + total.toFixed(2));
+            alert("Gracias por su compra")
+        }
+    }
+}
+
+comprarCursos()
